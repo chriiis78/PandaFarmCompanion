4dd52a903721be850f9eace30cff5c88e160d93d
Christophe Mei <c.mei@live.fr>
2020-06-16 17:59:41 +0200

    add General button and open url text field

--- PandaFarmCompanion/ViewController.swift
+++ PandaFarmCompanion/ViewController.swift
@@ -8,28 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var wifiButton: UIButton!
     @IBOutlet weak var settingsTextField: UITextField!
     @IBAction func wifiAction(_ sender: Any) {
         openSettings("WIFI")
     }
+    @IBAction func generalSettingsAction(_ sender: Any) {
+        openSettings("General")
+    }
     @IBAction func testAction(_ sender: Any) {
         openSettings("\(settingsTextField.text!)")
     }
+    @IBOutlet weak var browserTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        settingsTextField.delegate = self
+        browserTextField.delegate = self
     }
 
     func openSettings(_ pref: String) {
         if #available(iOS 13, *) {
             UIApplication.shared.open(URL(string: "App-prefs:\(pref)")!)
         } else {
-            UIApplication.shared.open(URL(string: "App-prefs:root:\(pref)")!)
+            UIApplication.shared.open(URL(string: "App-prefs:root=\(pref)")!)
         }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case settingsTextField:
+            openSettings(textField.text ?? "")
+        case browserTextField:
+            if let url = URL(string: textField.text ?? "") {
+                UIApplication.shared.open(url)
+            }
+        default:
+            break
+        }
+
+        return true
     }
 }
 
